e0fde7652f7cd0b5755e535b4e37753f2fc6d5b5
aditya-v22 <aditya.vaishanv2022@gmail.com>
2023-06-20 18:53:26 +0530

    Add header UI

---
+++ components/Header/index.tsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Header = () => {
+  return (
+    <div className='sticky'>
+      <div className='w-full flex justify-between items-center'>
+        <div className='flex items-center gap-8'>
+          <FontAwesomeIcon
+            icon={faArrowLeftLong}
+            color='gray'
+            size='3x'
+            className='hover:opacity-80 cursor-pointer'
+          />
+
+          <p className='text-4xl'>Instance Name</p>
+        </div>
+
+        <div className='flex gap-5'>
+          <div className='flex flex-col'>
+            <p className='text-3xl'>Node Moniker</p>
+            <p className='text-slate-500 text-sm'>21212323232322323232</p>
+          </div>
+
+          <div className='border-l border-gray-600' />
+
+          <button className='px-16 py-4 bg-green-800 rounded-2xl hover:bg-green-700'>
+            Start
+          </button>
+        </div>
+
+        <Link href='/'>
+          <Image
+            src='/sentinel-logo.png'
+            alt='Sentinel'
+            height={50}
+            width={200}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { Header } from '@/components/Header';
+
 export default function Home() {
-  return <main>NeoBase</main>;
+  return (
+    <main className='bg-black h-screen text-white'>
+      <div className='container mx-auto px-9 py-10'>
+        <Header />
+      </div>
+    </main>
+  );
 }
